d0d2803f644e5889a71107d7dc5c495953f03f93
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2016-01-04 14:22:00 -0200

    Added CONTNAME in the discovery rule.

--- docker_discover.py
+++ docker_discover.py
@@ -4,9 +4,9 @@ from docker import Client
 
 # A Simple module that returns ids for all docker containers.
 
-cli = Client('unix://var/run/docker.sock')
+cli = Client('http://127.0.0.1:2376')
 
-containers = [ {'#CONTAINER': container['Id']} 
+containers = [ {'#CONTAINER': container['Id'], '#CONTNAME': container['Names'][0]} 
                for container in cli.containers(all=True) ]
 
 print(json.dumps(containers))

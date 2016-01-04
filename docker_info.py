90d69c14a31b842c203f5dacc8e1e18352119fd5
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2016-01-04 15:06:03 -0200

    Added param to specify the server address.

--- docker_info.py
+++ docker_info.py
@@ -4,9 +4,9 @@ import argparse
 from docker import Client
 
 # A Simple module that returns attributes values for given ids.
-def get_container_attr(container_id, attr):
+def get_container_attr(container_id, attr, addr):
     # Find a container info and return desired attr.
-    cli = Client('unix://var/run/docker.sock')
+    cli = Client(addr)
     container = cli.inspect_container(container_id)
     print(json.dumps(container[attr]))
 
@@ -14,8 +14,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--cid', type=str, required=True)
     parser.add_argument('--attr', type=str, required=True)
+    parser.add_argument('--addr', type=str, required=False)
 
     args = parser.parse_args()
+
+    if not args.addr:
+        addr = 'http://127.0.0.1:2376'
     
-    get_container_attr(container_id=args.cid, attr=args.attr)
+    get_container_attr(container_id=args.cid, attr=args.attr, addr=addr)
 

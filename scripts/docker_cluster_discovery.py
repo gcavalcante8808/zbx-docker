3d526c0496ef40827c2bd706a91c17c009f7cb0e
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2017-04-14 00:35:47 -0300

    Closes #7.

--- scripts/docker_cluster_discovery.py
+++ scripts/docker_cluster_discovery.py
@@ -27,6 +27,13 @@ def discovery_nodes(cli):
     print(json.dumps({'data': nodes}))
 
 
+def extrode_multiple_urls(urls):
+    """ Return the last (right) url value """
+    if urls and len(urls) > 1:
+        return urls[-1]
+    
+    return urls
+
 def discovery_services(cli):
     services_list = cli.services.list()
 
@@ -34,7 +41,7 @@ def discovery_services(cli):
                  "{#SERVICE_ID}": item.id,
                  "{#SERVICE_NAME}": item.name,
                  "{#SERVICE_HTTPSUPPORT}": 
-                      item.attrs['Spec']['Labels'].get('com.df.serviceDomain', False)
+                      extrode_multiple_urls(item.attrs['Spec']['Labels'].get('com.df.serviceDomain', False))
               } for item in services_list]
 
     print(json.dumps({'data': services}))
